852313d6a4f5638c867933cc514ff6f6c11f339c
Annabelle Wang <64243610+awctw@users.noreply.github.com>
2022-01-11 14:03:25 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Welcome!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
